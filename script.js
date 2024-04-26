274242a56ef45a3d881659c0756c0c233b5c1a2a
Thyale MD <thyaledantas@gmail.com>
2024-04-26 15:34:03 -0300

    Adicionando a seção de destaques

--- script.js
+++ script.js
@@ -8,3 +8,31 @@ function FecharBarraLateral(){
     transparencia.style.display = "none"
     BarraLateral.style.display = "none"
 }
+
+
+var CoverDoVideoStranger = window.document.querySelector("img#CapaDeStranger");
+var pnomestranger = window.document.querySelectorAll("p.pnomepnomestranger")
+
+function AbrirVideoStranger(){
+    CoverDoVideoStranger.style.display = "none"
+    pnomestranger.style.display = "none"
+}
+
+function FecharVideoStranger(){
+    CoverDoVideoStranger.style.display = "block"
+    pnomestranger.style.display = "block"
+}
+
+
+var CoverDoVideoManifest = window.document.querySelector("img#CapaDeManifest");
+var pnomeManifest = window.document.querySelectorAll("p.pnomeManifest")
+
+function AbrirVideoManifest(){
+    CoverDoVideoManifest.style.display = "none"
+    pnomeManifest.style.display = "none"
+}
+
+function FecharVideoManifest(){
+    CoverDoVideoManifest.style.display = "block"
+    pnomeManifest.style.display = "block"
+}
